61af6d9d36f39aa4c6547aefb97afcc033b4e3c5
barbapapazes <e.soubiran25@gmail.com>
2024-04-26 09:20:27 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { Container, inject } from '@adonisjs/fold'
+
+async function main() {
+    class Database {}
+
+    @inject()
+    class UserService {
+      constructor(public db: Database) {}
+    }
+
+
+  console.log(UserService.containerInjections)
+
+  const container = new Container()
+  const service = await container.make(UserService)
+
+  console.log(service instanceof UserService)
+}
+
+main()
